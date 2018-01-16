6af243cd86c32b7e50eb025f93af1ca4765243ab
姚铭亮 <>
2018-01-16 10:36:53 +0800

    重新理解棋盘覆盖算法，修正算法代码

--- src/com/sumu/dispatch/ChessBoard.java
+++ src/com/sumu/dispatch/ChessBoard.java
@@ -15,12 +15,12 @@ public class ChessBoard {
 	 *            棋盘的大小
 	 * @param specialRow
 	 *            //特殊点的行下标
-	 * @param specialLow
+	 * @param specialCol
 	 *            //特殊点的列下标
 	 */
-	public static void chessBoard(int size, int specialRow, int specialLow) {
+	public static void chessBoard(int size, int specialRow, int specialCol) {
 		int[][] board = new int[size][size];// 初始化棋盘
-		chessBoard(board, specialRow, specialLow, 0, 0, size);
+		chessBoard(board, specialRow, specialCol, 0, 0, size);
 		printArray(board);
 	}
 
@@ -30,70 +30,76 @@ public class ChessBoard {
 	 *            棋盘
 	 * @param specialRow
 	 *            特殊点的行下标
-	 * @param specialLow
+	 * @param specialCol
 	 *            特殊点的列下标
 	 * @param leftTopRow
 	 *            小棋盘的左上角顶点行下标
-	 * @param leftToplow
+	 * @param leftTopCol
 	 *            小棋盘的左上角顶点列下标
 	 * @param size
 	 *            棋盘大小
 	 */
-	public static void chessBoard(int[][] board, int specialRow, int specialLow, int leftTopRow, int leftToplow,
+	public static void chessBoard(int[][] board, int specialRow, int specialCol, int leftTopRow, int leftTopCol,
 			int size) {
 		if (size == 1) {
 			return;
 		}
-		int subSize = size / 2;
 		count++;
-		// 如果特殊点的位置在左上角
-		if (specialRow <leftTopRow+subSize && specialLow < leftTopRow+subSize) {
-			chessBoard(board, specialRow, specialLow, leftTopRow, leftToplow, subSize);
+		int t=count;//L型编号
+		int subSize = size / 2;
+		// 如果特殊点的位置在左上角矩阵里面
+		if (specialRow <leftTopRow+subSize && specialCol < leftTopCol+subSize) {
+			chessBoard(board, specialRow, specialCol, leftTopRow, leftTopCol, subSize);
 		} else {
-			//覆盖右下角
-			board[leftTopRow+subSize][leftToplow+subSize]=count;
-			//再以右下角那个点作为特殊点继续
-			chessBoard(board, leftTopRow+subSize, leftTopRow+subSize, leftTopRow, leftTopRow, subSize);
+			//如果不在左上角矩阵里，那么就以左上角矩阵的右下角作为特殊点
+			board[leftTopRow+subSize-1][leftTopCol+subSize-1]=t;
+			//再以右下角那个点作为特殊点,继续填充左上角矩阵
+			chessBoard(board, leftTopRow+subSize-1, leftTopCol+subSize-1, leftTopRow, leftTopCol, subSize);
 		}
 
-		// 如果特殊点的位置在右上角
-		if (specialRow < leftTopRow+subSize && specialLow >= leftTopRow+subSize) {
-			chessBoard(board, specialRow, specialLow, leftTopRow, leftToplow, subSize);
+		// 如果特殊点的位置在右上角矩阵里面
+		if (specialRow < leftTopRow+subSize && specialCol >= leftTopCol+subSize) {
+			chessBoard(board, specialRow, specialCol, leftTopRow, leftTopCol+subSize, subSize);
 		} else {
-			//覆盖左下角
-			board[leftTopRow+subSize][leftToplow]=count;
-			chessBoard(board, leftTopRow+subSize, leftToplow, leftTopRow, leftToplow, subSize);
+			//如果不在右上角矩阵里，那么就以右上角矩阵的左下角作为特殊点
+			board[leftTopRow+subSize-1][leftTopCol+subSize]=t;
+			//再以左下角那个点作为特殊点,继续填充右上角矩阵
+			chessBoard(board, leftTopRow+subSize-1, leftTopCol+subSize, leftTopRow, leftTopCol+subSize, subSize);
 		}
 
-		// 如果特殊点的位置在左下角
-		if (specialRow >= leftTopRow+subSize && specialLow < leftTopRow+subSize) {
-			chessBoard(board, specialRow, specialLow, leftTopRow, leftToplow, subSize);
+		// 如果特殊点的位置在左下角矩阵里面
+		if (specialRow >= leftTopRow+subSize && specialCol < leftTopCol+subSize) {
+			chessBoard(board, specialRow, specialCol, leftTopRow+subSize, leftTopCol, subSize);
 		} else {
-			//覆盖右上角
-			board[leftTopRow][leftToplow+subSize]=count;
-			chessBoard(board, leftTopRow, leftToplow+subSize, leftTopRow, leftToplow, subSize);
+			//如果不在左下角矩阵里，那么就以左下角矩阵的右上角作为特殊点
+			board[leftTopRow+subSize][leftTopCol+subSize-1]=t;
+			//再以右上角那个点作为特殊点,继续填充左下角矩阵
+			chessBoard(board, leftTopRow+subSize, leftTopCol+subSize-1, leftTopRow+subSize, leftTopCol, subSize);
 		}
 
-		// 如果特殊点的位置在右下角
-		if (specialRow >= leftTopRow+subSize && specialLow >= leftTopRow+subSize) {
-			chessBoard(board, specialRow, specialLow, leftTopRow, leftTopRow, subSize);
+		// 如果特殊点的位置在右下角矩阵里面
+		if (specialRow >= leftTopRow+subSize && specialCol >= leftTopCol+subSize) {
+			chessBoard(board, specialRow, specialCol, leftTopRow+subSize, leftTopCol+subSize, subSize);
 		} else {
-			//覆盖左上角
-			board[leftTopRow][leftToplow]=count;
-			chessBoard(board, leftTopRow, leftToplow, leftTopRow, leftToplow, subSize);
+			//如果不在右下角矩阵里，那么就以右下角矩阵的左上角作为特殊点
+			board[leftTopRow+subSize][leftTopCol+subSize]=t;
+			//再以左上角那个点作为特殊点,继续填充右下角矩阵
+			chessBoard(board, leftTopRow+subSize, leftTopCol+subSize, leftTopRow+subSize, leftTopCol+subSize, subSize);
 		}
+		
 	}
 
 	public static void printArray(int[][] board) {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				System.out.print(board[i][j] + " ");
+				System.out.print(board[i][j] + "  ");
 			}
 			System.out.println("");
 		}
+		System.out.println("");
 	}
 
 	public static void main(String[] args) {
-		ChessBoard.chessBoard(4, 0, 0);
+		ChessBoard.chessBoard(4, 1, 1);
 	}
 }
